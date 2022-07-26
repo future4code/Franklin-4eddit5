194f05baabddba2321345b29e7002019a875c85d
Denilson Santos <denilson.santos@grupoboticario.com.br>
2022-07-26 04:17:35 -0300

    Criando requests e hooks

--- labeddit/src/pages/Feed/index.jsx
+++ labeddit/src/pages/Feed/index.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { BASE_URL } from "../../constantes/urls";
+import { useProtectedPage } from "../../hooks/useProtectdPage";
+import { useRequestData } from "../../hooks/useRequestData";
 
 export const Feed = () => {
-  return <>Página de Feed</>;
+ 
+  useProtectedPage()
+
+  const postes = useRequestData([], `${BASE_URL}/posts`)
+
+  console.log(postes);
+
+  return (
+    postes.map( (post) => {
+      return (
+        <div key={post.id}> 
+          <p>{post.body}</p>
+          <p>{post.title}</p>
+          <p>{post.commentCount}</p>
+          <p>{post.createdAt}</p>
+          <p>{post.username}</p>
+          <p>{post.userId}</p>
+          <p>{post.userVote}</p>
+          <p>{post.voteSum}</p>
+        </div>
+      )
+
+    })
+  );
 };
