e70d0ed3fb0cf2c2358a56f8d7deda43ea7d78f7
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2022-07-26 11:08:53 -0300

    feat: header (#9)

--- labeddit/src/pages/Feed/index.jsx
+++ labeddit/src/pages/Feed/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Header from "../../components/Header";
 
 export const Feed = () => {
-  return <>Página de Feed</>;
+  return (
+    <>
+      <Header LinkName={"Entrar"} />
+    </>
+  );
 };
