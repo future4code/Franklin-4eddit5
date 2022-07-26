67b01dbd5d4014bcb26dfcbce2a878c0826fef98
Leonardo Santino <leonardosantino@live.com>
2022-07-26 12:44:58 -0300

    feat: create page feed

--- labeddit/src/pages/Feed/index.jsx
+++ labeddit/src/pages/Feed/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Header from "../../components/Header";
+import { Container, FormContainer, Form, Line, PostsContainer } from "./styles";
+import TextArea from "../../components/Form/TextArea";
+import { ButtonSecondary } from "../../components/Form/Button";
 
 export const Feed = () => {
   return (
-    <>
-      <Header LinkName={"Entrar"} />
-    </>
+    <Container>
+      <Header LinkName={"Logout"} />
+      <FormContainer>
+        <Form>
+          <TextArea label={"Escreva seu Post..."} />
+          <ButtonSecondary name={"Postar"} />
+          <Line />
+        </Form>
+      </FormContainer>
+      <PostsContainer>
+        <div style={{ background: "grey", textAlign: "center" }}>PostCard</div>
+        <div style={{ background: "grey", textAlign: "center" }}>PostCard</div>
+        <div style={{ background: "grey", textAlign: "center" }}>PostCard</div>
+      </PostsContainer>
+    </Container>
   );
 };
