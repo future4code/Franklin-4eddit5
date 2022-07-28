9761d6f8d3a29b34a07510f0156673d08ce68d5e
Ítalo Fernandes <italo.avelino@outlook.com.br>
2022-07-28 10:51:17 -0300

    feat: add props to component

--- labeddit/src/components/PostCard/index.jsx
+++ labeddit/src/components/PostCard/index.jsx
@@ -1,25 +1,23 @@
 import * as S from "./style"
 
-export const PostCard = () => {
+export const PostCard = (props) => {
   return (
     <S.Card>
-        <S.MessageAuthor>Enviado por: labaluno83</S.MessageAuthor>
-        <S.MessageContent>
-            Porque a maioria dos desenvolvedores usam Linux? ou as empresas de tecnologia usam Linux ?
-        </S.MessageContent>
+        <S.MessageAuthor>Enviado por: {props.username}</S.MessageAuthor>
+        <S.MessageContent>{props.message}</S.MessageContent>
         <S.Interaction>
           <S.VoteWrapper>
-            <S.ButtonVote>
+            <S.ButtonVote onClick={props.positiveVote}>
               <S.ImageVote src="/images/positiveVote.png" alt="Voto positivo"/>
             </S.ButtonVote>
-            <S.VoteText>1.2k</S.VoteText>
-            <S.ButtonVote>
+            <S.VoteText>{props.vote}</S.VoteText>
+            <S.ButtonVote onClick={props.negativeVote}>
               <S.ImageVote src="/images/negativeVote.png" alt="Voto negativo"/>
             </S.ButtonVote>
           </S.VoteWrapper>
           <S.CommentWrapper>
             <S.ImageComment src="/images/Comment.png" alt="Comentarios"/>
-            <S.CommentText>132</S.CommentText>
+            <S.CommentText>{props.comment}</S.CommentText>
           </S.CommentWrapper>
         </S.Interaction>
     </S.Card>
